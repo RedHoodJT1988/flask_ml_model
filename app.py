ee287af8c4522ce4a02b1afe3d48557d412f82c0
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-04-12 14:20:50 -0700

    heroku setup

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, url_for, redirect, render_template
 import pandas as pd
 import pickle
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 model = pickle.load(open("example_weights_knn.pkl", "rb"))
 
